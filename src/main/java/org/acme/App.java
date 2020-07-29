01971f7a3cb38cd21c175c70b3726f30cd32f1b5
aurea munoz <amunozhe@redhat.com>
2020-07-29 13:57:08 +0200

    first commit: application framework less

---
+++ src/main/java/org/acme/App.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) throws IOException {
+        int serverPort = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        server.createContext("/api/hello", (exchange -> {
+            String respText = "Hello FrameWorkless world!";
+            exchange.sendResponseHeaders(200, respText.getBytes().length);
+            OutputStream output = exchange.getResponseBody();
+            output.write(respText.getBytes());
+            output.flush();
+            exchange.close();
+        }));
+        server.setExecutor(null); // creates a default executor
+        System.out.println("Listening in port "+serverPort);
+        server.start();
+    }
+
+}
